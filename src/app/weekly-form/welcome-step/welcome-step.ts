90fd3bcafd6eea072732cf86d85563ffc9348080
mikalaipalesski <mikalai.hlushakou@gmail.com>
2026-04-26 04:52:16 +0200

    First page imit

--- src/app/weekly-form/welcome-step/welcome-step.ts
+++ src/app/weekly-form/welcome-step/welcome-step.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-welcome-step",
@@ -6,4 +7,10 @@ import { Component } from "@angular/core";
   templateUrl: "./welcome-step.html",
   styleUrl: "./welcome-step.scss",
 })
-export class WelcomeStepComponent {}
+export class WelcomeStepComponent {
+  private router = inject(Router);
+
+  start() {
+    this.router.navigate(["/weekly-form/step1"]);
+  }
+}
