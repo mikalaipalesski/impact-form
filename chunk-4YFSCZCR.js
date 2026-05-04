7ec6895010d2a9defc5b5341044f2c282f03574a
mikalaipalesski <mikalai.hlushakou@gmail.com>
2026-05-04 17:21:13 +0200

    Auto-generated commit

---
+++ chunk-4YFSCZCR.js
@@ -0,0 +1,1 @@
+import{Ra as n,hb as o,ib as i,ob as m}from"./chunk-DEZQGVJY.js";var a=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-review-submit"]],decls:2,vars:0,template:function(e,r){e&1&&(o(0,"p"),m(1,"review-submit works!"),i())},encapsulation:2})};export{a as ReviewSubmitComponent};
